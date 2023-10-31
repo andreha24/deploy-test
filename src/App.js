5bb5507df5fb295de59b192b4c67ae7918c4dac3
Andrey Sherstiuk <sherstukandrey15@gmail.com>
2023-10-31 13:07:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [allUsers, setAllUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api")
+      .then(function (response) {
+        setAllUsers(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {allUsers.map(({id, name}) => (
+        <div key={id}>
+          <h1>{id}</h1>
+          <p>{name}</p>
+        </div>
+      ))}
     </div>
   );
 }
